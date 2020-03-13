6ad5498ad2020467c45e7fda5afa2213acd5b5ee
jakub104 <jakubzlezena@wp.pl>
2020-03-13 18:05:08 +0100

    Updates

--- precache-manifest.ef8d0203ce643d6396a8c75f33ed0683.js
+++ precache-manifest.ef8d0203ce643d6396a8c75f33ed0683.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2fc3348e6ef28f297a032722202d595",
+    "revision": "03399d54d6eaf2c22e7e70d98847bb4f",
     "url": "/easy-tic-tac-toe/index.html"
   },
   {
-    "revision": "dc54dd7e43a47fc642b4",
+    "revision": "ab56d5d37ceb42cda515",
     "url": "/easy-tic-tac-toe/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "84df24cae0e025517e0d",
-    "url": "/easy-tic-tac-toe/static/js/2.efee2cd0.chunk.js"
+    "revision": "e13a1ef6eb2ef5381896",
+    "url": "/easy-tic-tac-toe/static/js/2.0a3a74d9.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/easy-tic-tac-toe/static/js/2.efee2cd0.chunk.js.LICENSE.txt"
+    "url": "/easy-tic-tac-toe/static/js/2.0a3a74d9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc54dd7e43a47fc642b4",
-    "url": "/easy-tic-tac-toe/static/js/main.9e292509.chunk.js"
+    "revision": "ab56d5d37ceb42cda515",
+    "url": "/easy-tic-tac-toe/static/js/main.76b7c788.chunk.js"
   },
   {
     "revision": "151c362c8416f28ce825",
