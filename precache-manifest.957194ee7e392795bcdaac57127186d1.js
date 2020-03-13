c33a2ef6764b7a08dadcb6e0e4de09008efb2751
jakub104 <jakubzlezena@wp.pl>
2020-03-13 17:58:18 +0100

    Updates

---
+++ precache-manifest.957194ee7e392795bcdaac57127186d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fc3348e6ef28f297a032722202d595",
+    "url": "/easy-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "dc54dd7e43a47fc642b4",
+    "url": "/easy-tic-tac-toe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "84df24cae0e025517e0d",
+    "url": "/easy-tic-tac-toe/static/js/2.efee2cd0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/easy-tic-tac-toe/static/js/2.efee2cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc54dd7e43a47fc642b4",
+    "url": "/easy-tic-tac-toe/static/js/main.9e292509.chunk.js"
+  },
+  {
+    "revision": "151c362c8416f28ce825",
+    "url": "/easy-tic-tac-toe/static/js/runtime-main.054c2d3d.js"
+  }
+]);
